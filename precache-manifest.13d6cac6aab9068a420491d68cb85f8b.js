7ae85dfc1ee5e5812f5870cdb31141daf688499f
jeevanantham123 <51525786+jeevanantham123@users.noreply.github.com>
2020-06-01 20:00:19 +0530

    Updates

---
+++ precache-manifest.13d6cac6aab9068a420491d68cb85f8b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5ff509bc6064e2fea21bd5ddde9b3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57394c585cac96b59610",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b29e7345c1fc0ed6d6e1",
+    "url": "./static/css/main.d6c0b53c.chunk.css"
+  },
+  {
+    "revision": "57394c585cac96b59610",
+    "url": "./static/js/2.7d12c766.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "./static/js/2.7d12c766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29e7345c1fc0ed6d6e1",
+    "url": "./static/js/main.a281d2dc.chunk.js"
+  },
+  {
+    "revision": "f35dbb30bb93cebd5c82",
+    "url": "./static/js/runtime-main.f63e6028.js"
+  },
+  {
+    "revision": "6e8c663271799bc71481b3f7aab69f37",
+    "url": "./static/media/logo.6e8c6632.png"
+  },
+  {
+    "revision": "e021b519668665a109e6d1f2d6141b9c",
+    "url": "./static/media/rand1.e021b519.jpg"
+  },
+  {
+    "revision": "0e28de83002e76eefdca9d0735126d2f",
+    "url": "./static/media/rand2.0e28de83.jpg"
+  },
+  {
+    "revision": "04ad679464c6c3a05dde9554730316f5",
+    "url": "./static/media/rand3.04ad6794.jpg"
+  },
+  {
+    "revision": "7f133fce4bd1120fa6ee0b7edf217fe1",
+    "url": "./static/media/rand4.7f133fce.jpg"
+  },
+  {
+    "revision": "9f167a6e798df13c4d26d41526e2d0e2",
+    "url": "./static/media/rand5.9f167a6e.jpg"
+  }
+]);
