80587762c1363e293dcf2dabf645b65a451f201c
jeevanantham123 <51525786+jeevanantham123@users.noreply.github.com>
2020-06-01 19:43:20 +0530

    Updates

---
+++ precache-manifest.52665a7d1ae7a163e40b21fd284af6e8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a04fe73b7035b59d23940d412a1d00",
+    "url": "/instagram-replica/index.html"
+  },
+  {
+    "revision": "af9a138394558c00bbad",
+    "url": "/instagram-replica/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "db3232310733c8aacbec",
+    "url": "/instagram-replica/static/css/main.3ed569fe.chunk.css"
+  },
+  {
+    "revision": "af9a138394558c00bbad",
+    "url": "/instagram-replica/static/js/2.0f4e1e32.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/instagram-replica/static/js/2.0f4e1e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3232310733c8aacbec",
+    "url": "/instagram-replica/static/js/main.1a4bfc1e.chunk.js"
+  },
+  {
+    "revision": "725c69e87852817bd177",
+    "url": "/instagram-replica/static/js/runtime-main.b27fc427.js"
+  },
+  {
+    "revision": "6e8c663271799bc71481b3f7aab69f37",
+    "url": "/instagram-replica/static/media/logo.6e8c6632.png"
+  },
+  {
+    "revision": "e021b519668665a109e6d1f2d6141b9c",
+    "url": "/instagram-replica/static/media/rand1.e021b519.jpg"
+  },
+  {
+    "revision": "0e28de83002e76eefdca9d0735126d2f",
+    "url": "/instagram-replica/static/media/rand2.0e28de83.jpg"
+  },
+  {
+    "revision": "04ad679464c6c3a05dde9554730316f5",
+    "url": "/instagram-replica/static/media/rand3.04ad6794.jpg"
+  },
+  {
+    "revision": "7f133fce4bd1120fa6ee0b7edf217fe1",
+    "url": "/instagram-replica/static/media/rand4.7f133fce.jpg"
+  },
+  {
+    "revision": "9f167a6e798df13c4d26d41526e2d0e2",
+    "url": "/instagram-replica/static/media/rand5.9f167a6e.jpg"
+  }
+]);
